190771218161716884631dceb9b35a76d189e5bd
Daniel Garza <58958492+danielgarzaf@users.noreply.github.com>
2020-07-10 14:29:26 -0500

    Búsqueda de imagenes
    Solicita el input del usuario desde la terminal para iniciar la búsqueda

---
+++ Equipo3.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+# Va a la dirección especificada
+driver.get("http://image-net.org")
+
+# Encuentra el elemento para poder hacer la búsqueda
+search = driver.find_element_by_id("searchbox")
+
+# Solicita el input del usuario, lo escribe y hace enter
+search.send_keys(input("Buscar: "))
+search.send_keys(Keys.RETURN)
+
+# Cierra la ventana del browser
+# driver.quit()
