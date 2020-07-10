9da938238ba6cbb3f033226caed8e25a4eb3ac27
danielgarzaf <daniel_gf97@hotmail.com>
2020-07-10 17:01:06 -0500

    Cambios para que funcione con shutterstock

--- Equipo3.py
+++ Equipo3.py
@@ -6,10 +6,10 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 # Va a la dirección especificada
-driver.get("http://image-net.org")
+driver.get("https://www.shutterstock.com")
 
 # Encuentra el elemento para poder hacer la búsqueda
-search = driver.find_element_by_id("searchbox")
+search = driver.find_element_by_name("searchterm")
 
 # Solicita el input del usuario, lo escribe y hace enter
 search.send_keys(input("Buscar: "))
