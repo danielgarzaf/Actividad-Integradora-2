05f2e1e9f9d0ae7fff59a6d259d5da30aec51491
Daniel Garza <58958492+danielgarzaf@users.noreply.github.com>
2020-07-10 17:48:43 -0500

    Update Equipo3.py

--- Equipo3.py
+++ Equipo3.py
@@ -5,11 +5,15 @@ import os
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+image_net = "http://image-net.org"
+shutterstock = "https://www.shutterstock.com"
+
 # Va a la dirección especificada
-driver.get("https://www.shutterstock.com")
+driver.get(image_net)
 
 # Encuentra el elemento para poder hacer la búsqueda
-search = driver.find_element_by_name("searchterm")
+# search = driver.find_element_by_name("searchterm") # Si se está usando shutterstock
+search = driver.find_element_by_id("searchbox") # Si se está usando image-net
 
 # Solicita el input del usuario, lo escribe y hace enter
 search.send_keys(input("Buscar: "))
