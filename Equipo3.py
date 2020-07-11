547b655e164ab66a80cd29d257f88641dbba3901
Daniela Garcia <mdgarciaaguilar@hotmail.com>
2020-07-10 22:04:06 -0500

    metodo para descargar imagenes de su url listo

--- Equipo3.py
+++ Equipo3.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import os
+import numpy as np
+import urllib
+import cv2
+
+# DRIVER_PATH = './chromedriver'
+# driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
+
+# Función para descargar una imagen desde su URL
+def downloadImg(url):
+    response = urllib.request.urlopen(url)
+    img = np.asarray(bytearray(response.read()), dtype="uint8")
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    return img
+
+
 
 image_net = "http://image-net.org"
 shutterstock = "https://www.shutterstock.com"
@@ -19,5 +34,34 @@ search = driver.find_element_by_id("searchbox") # Si se está usando image-net
 search.send_keys(input("Buscar: "))
 search.send_keys(Keys.RETURN)
 
+# Solicita el input del usuario, lo escribe y hace enter
+search.send_keys(input("Buscar: "))
+search.send_keys(Keys.RETURN)
+
+# Selecciona el primer resultado de la búsqueda y da click
+driver.find_element_by_xpath("//span[@class='result_synset']").click()
+
+
+# Obtiene la lista de urls de las imágenes
+
+
+
 # Cierra la ventana del browser
 # driver.quit()
+
+
+# Lista de URLs
+urls = [
+    "https://pyimagesearch.com/wp-content/uploads/2015/01/google_logo.png",
+    "https://pyimagesearch.com/wp-content/uploads/2014/12/adrian_face_detection_sidebar.png",
+]
+
+
+index = 0
+# Recorre la lista de URLs y descarga cada imagen
+for url in urls:
+    img = downloadImg(url)
+    name = 'image'+str(index)+'.jpg'
+    cv2.imwrite(name,img)
+    cv2.destroyAllWindows()
+    index = index + 1
